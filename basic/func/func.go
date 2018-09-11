a8f461136943411bf3ac2f61a57d6b27bf7fab3e
ZzXxL1994 <1207346575@qq.com>
2018-09-11 09:34:00 +0800

    study go basic

--- basic/func/func.go
+++ basic/func/func.go
@@ -7,6 +7,18 @@ import (
 	"runtime"
 )
 
+// 函数要点
+// 1. 返回值类型写在最后面
+// 2. 可返回多个值
+// 3. 函数作为参数
+// 4. 没有默认参数、可选参数等，只有可变参数列表
+
+// 值传递和引用传递
+// cpp中可以值传递也可以引用传递（&）
+// python和java除了系统内建类型是值传递以外，都是引用传递
+// golang只有值传递，要改变值必须用指针参数
+
+// 下划线表示变量不想用，其他变量名定义了不用会编译不过
 func eval(a, b int, op string) (int, error) {
 	switch op {
 	case "+":
@@ -24,10 +36,26 @@ func eval(a, b int, op string) (int, error) {
 	}
 }
 
+// 两个返回值的三种写法
+// 1.
+// 13 / 3 = 4 ... 1
+//func div(a, b int) (int, int) {
+//  return a / b, a % b
+//}
+
+// 2. 函数返回多个值时可以起名字，对于调用者而言没有区别
 func div(a, b int) (q, r int) {
 	return a / b, a % b
 }
 
+// 3. 也可以，但不推荐。仅用于非常简单的函数
+//func div(a, b int) (q, r int) {
+//  q = a / b
+//  r = a % b
+//  return
+//}
+
+// 函数作为参数
 func apply(op func(int, int) int, a, b int) int {
 	p := reflect.ValueOf(op).Pointer()
 	opName := runtime.FuncForPC(p).Name()
@@ -36,6 +64,7 @@ func apply(op func(int, int) int, a, b int) int {
 	return op(a, b)
 }
 
+// 可变参数列表
 func sum(numbers ...int) int {
 	s := 0
 	for i := range numbers {
